fbc9ff172d20f1d44a415d91d46372e7ae9d5690
alinaandreeamanolea20 <73846716+alinaandreeamanolea20@users.noreply.github.com>
2021-03-24 22:48:29 +0200

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,13 +1,12 @@
-# create a weather report for the current day (& hour) for a city (Bucharest, Rome, ...)
-# you can use the API from https://www.weatherbit.io/
-# url for current day: https://api.weatherbit.io/v2.0/current
-# params: city and key; for key you can use mine 1ab5e9499298416b97674b2facdaeb1c or make a free account on the site
-# save the weather report in a file & make a class which can reconstruct the object from the file
-# every weather report is appended to its city, we never delete a previous day
-# weather report should contain a description of the weather, temperature, clouds metric & sunset time
+# A weather report for the current day (& hour) for a city (ex: Bucharest, Rome, etc...)
+# I used API from https://www.weatherbit.io/  and  url for current day: https://api.weatherbit.io/v2.0/current
+# Params: city and key; for key you can use mine 1ab5e9499298416b97674b2facdaeb1c 
+# Weather report should contain a description of the weather, temperature, clouds metric & sunset time
+
+
 import requests
 import json
-#
+
 
 def print_weather(city):
     url = 'https://api.weatherbit.io/v2.0/current'
